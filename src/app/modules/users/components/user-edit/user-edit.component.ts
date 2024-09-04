ea272829171cb4e57a1ce1d9469581e2543e3ccd
mhpm <michelle.perez@bldr.com>
2024-09-04 12:31:30 -0600

    ✨ - Reactive Form Ready on edit user

--- src/app/modules/users/components/user-edit/user-edit.component.ts
+++ src/app/modules/users/components/user-edit/user-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { IUser } from '../../types/user.interface';
 
 @Component({
@@ -6,17 +7,50 @@ import { IUser } from '../../types/user.interface';
   templateUrl: './user-edit.component.html',
   styleUrl: './user-edit.component.scss',
 })
-export class UserEditComponent {
-  @Input() user: IUser = { id: '', first_name: '', last_name: '', email: '' };
+export class UserEditComponent implements OnInit, OnChanges {
+  @Input() user: IUser = { id: '', first_name: '', last_name: '', email: '', avatar: '' };
   @Output() onClose = new EventEmitter();
   @Output() onSave = new EventEmitter<IUser>();
+  editForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.editForm = this.fb.group({
+      id: ['', [Validators.required]],
+      first_name: ['', [Validators.required, Validators.minLength(2)]],
+      last_name: ['', [Validators.required, Validators.minLength(2)]],
+      email: ['', [Validators.required, Validators.email]],
+      avatar: ['']
+    });
+  }
+
+  ngOnInit(): void {
+    if (this.user) {
+      this.editForm.patchValue(this.user);
+    }
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    // If the dialog is opened with a user, populate the form
+    if (changes['user'] && changes['user'].currentValue) {
+      this.editForm.patchValue(this.user);
+    }
+  }
 
   onSubmit() {
-    this.onSave.emit(this.user);
-    this.onClose.emit();
+    if (this.editForm.valid) {
+      this.onSave.emit(this.editForm.value);
+      this.onClose.emit();
+    }
   }
 
   onCancel() {
+    this.editForm.reset({
+      id: '',
+      first_name: '',
+      last_name: '',
+      email: '',
+      avatar: ''
+    });
     this.onClose.emit();
   }
 }
